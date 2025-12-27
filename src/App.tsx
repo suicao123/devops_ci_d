9422dde67bdfe74c8a43241eee2bf79603b3fc53
suicao123 <chongtuanphat2004@gmail.com>
2025-12-27 12:07:18 +0700

    fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 
 const App: React.FC = () => {
   return (
